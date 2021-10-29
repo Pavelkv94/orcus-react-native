05d0c6daf0a3802e5440e57a3340b73f1562bc90
pnkazlou <pnkazlou@ibagroup.eu>
2021-10-29 15:14:06 +0300

    create app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,45 @@
+import { extendTheme, NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import Main from './components/Main';
 import { store } from './redux/store';
 
+
+const theme = extendTheme({
+	colors: {
+		// Add new color
+		primary: {
+			50: '#ecfeff',
+			100: '#cffafe',
+			200: '#a5f3fc',
+			300: '#67e8f9',
+			400: '#22d3ee',
+			500: '#06b6d4',
+			600: '#0891b2',
+			700: '#0e7490',
+			800: '#155e75',
+			900: '#164e63',
+		},
+		// Redefinig only one shade, rest of the color will remain same.
+		amber: {
+			400: '#d97706',
+		},
+	},
+	config: {
+		// Changing initialColorMode to 'dark'
+		initialColorMode: 'dark',
+	},
+});
+
 export default function App() {
 
 	return (
 		<Provider store={store}>
 			<SafeAreaView style={styles.container}>
+			<NativeBaseProvider theme={theme}>
 				<Main />
+				</NativeBaseProvider>
 			</SafeAreaView>
 		</Provider>
 	);
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, View, } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategoriesTC } from '../redux/categoriesReducer';
-import { getShortPostsTC } from '../redux/postsReducer';
+import { getShortPostsTC, PostType } from '../redux/postsReducer';
+import { NativeBaseProvider, Box, extendTheme, Menu, HamburgerIcon, ScrollView, Center, Heading, VStack, Button } from 'native-base';
+import { getPostTC } from '../redux/filterReducer';
+import { AppStateType } from '../redux/store';
+import Markdown from 'react-native-markdown-text'
 
 export default function Main() {
-	const categories = useSelector<any, Array<any>>(state => state.categories)
+	const categories = useSelector<any, Array<any>>(state => state.categories);
 	const dispatch = useDispatch();
+	const posts = useSelector<any, Array<any>>(state => state.posts.shortPosts);
+	const post = useSelector<AppStateType, PostType>(state => state.filter);
 
 	useEffect(() => {
 		dispatch(getCategoriesTC());
@@ -16,13 +22,105 @@ export default function Main() {
 		dispatch(getShortPostsTC());
 	}, [dispatch]);
 
-	return (
+	const [currentPage, setCurrentPage] = useState<any>(null);
+	// const [currentArticle, setCurrentArticle] = useState(null);
 
-		<View style={styles.container}>
-			{categories.map(c => <Text>{c.title}</Text>)}
-		</View>
+	if (currentPage !== null && currentPage !== 'read') {
+		return (
+			<ScrollView _contentContainerStyle={{
+				px: "20px",
+				mb: "4",
+				minW: "72",
+			}} >
 
-	);
+				<Center mt="3" mb="4">
+					<Box h="90%" w='300'>
+						<Menu
+							// closeOnSelect={false}
+							w="190"
+							onOpen={() => console.log("opened")}
+							onClose={() => console.log("closed")}
+							trigger={(triggerProps) => {
+								return (
+									<Pressable {...triggerProps}>
+										<HamburgerIcon bg="primary.500" />
+									</Pressable>
+								)
+							}}
+						>
+							{categories.map(c => <Menu.Item bg="primary.500" key={c._id} onPress={() => { setCurrentPage(c.title); console.log(c.title) }}>{c.title}</Menu.Item>)}
+						</Menu>
+					</Box>
+					<Heading fontSize="xl" color="black">{currentPage}</Heading>
+				</Center>
+				<VStack flex="1" space={2}>
+					{posts.map(p => p.category === currentPage &&
+						<Button size="sm" key={p._id} onPress={() => { setCurrentPage('read'); dispatch(getPostTC(p._id)) }}>
+							<Text>{p.title}</Text>
+						</Button>
+					)}
+				</VStack>
+			</ScrollView>
+		);
+	} else if (currentPage === 'read') {
+		return (
+			<ScrollView _contentContainerStyle={{
+				px: "20px",
+				mb: "4",
+				minW: "72",
+			}} >
+
+				<Center mt="3" mb="4">
+					<Box h="90%" w='300'>
+						<Menu
+							// closeOnSelect={false}
+							w="190"
+							onOpen={() => console.log("opened")}
+							onClose={() => console.log("closed")}
+							trigger={(triggerProps) => {
+								return (
+									<Pressable {...triggerProps}>
+										<HamburgerIcon bg="primary.500" />
+									</Pressable>
+								)
+							}}
+						>
+							{categories.map(c => <Menu.Item bg="primary.500" key={c._id} onPress={() => { setCurrentPage(c.title); console.log(c.title) }}>{c.title}</Menu.Item>)}
+						</Menu>
+					</Box>
+				</Center>
+				<Markdown styles={styles}>{post.text}</Markdown>
+			</ScrollView>
+		);
+	} else
+
+		return (
+
+			<View style={styles.container}>
+				<Box h="90%" w='300'>
+					<Menu
+						// closeOnSelect={false}
+						w="190"
+						onOpen={() => console.log("opened")}
+						onClose={() => console.log("closed")}
+						trigger={(triggerProps) => {
+							return (<View>
+								<Pressable {...triggerProps}>
+									<HamburgerIcon bg="primary.500" />
+
+								</Pressable>
+								</View>
+							)
+						}}
+					>
+						{categories.map(c => <Menu.Item bg="primary.500" key={c._id} onPress={() => { setCurrentPage(c.title); console.log(c.title) }}>{c.title}</Menu.Item>)}
+					</Menu>
+				</Box>
+			</View>
+
+		);
+
+
 }
 
 const styles = StyleSheet.create({
@@ -30,6 +128,18 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'center'
+	},
+	heading1: {
+		fontSize: 22,
+	},
+	strong: {
+		fontSize: 18,
+	},
+	paragraph: {
+		fontSize: 14,
+	},
+	view: {
+		borderWidth: 1,
 	},
 });
